a23deac4f65e11460bcc4b67258b2770b8fde280
Abraham Rubinestock <abbrubin150@gmail.com>
2026-09-24 17:38:55 +0300

    Build a signed non-debuggable release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,13 +10,24 @@ android {
         applicationId = "com.wavesys.pixelphysics"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         release {
             isMinifyEnabled = false
+            isDebuggable = false
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
